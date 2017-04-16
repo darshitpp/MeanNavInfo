0181e6912ae1a6f67fc3ddc78a90962419d9e46e
Darshit <darshit12@yahoo.co.in>
2017-04-16 19:10:20 +0530

    find

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get("/api/navInfo/all", function(req, res) {
 app.get("/api/navInfo", function(req, res) {
 
     var sc = req.query.schemeCode;
-    db.collection(NAV_COLLECTION).findOne({ "Scheme Code": sc }, function(err, doc) {
+    db.collection(NAV_COLLECTION).find({ "Scheme Code": sc }, function(err, doc) {
         if (err) {
             handleError(res, err.message, "Failed to get Nav Info.");
         } else {
